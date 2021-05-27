76632f8e5e03479c6071e19788437b6170e1ffb4
crupy <wlxh95@naver.com>
2021-05-27 23:52:46 +0900

    chapter4 for,while,do while

---
+++ chapter4/RoopEx1.java
@@ -0,0 +1,19 @@
+package chapter4;
+
+public class RoopEx1 {
+
+	public static void main(String[] args) {
+		
+		int i;
+		int sum = 0;
+		
+		for(i = 0; i <= 100; i++) {                                 //for(초기조건;조건;처리부분을,로 두개넣을수 있다(ex)i++,i++)
+			sum += i;
+			System.out.println("sum : " + sum + ", " + "i : " + i);
+		}
+		
+		System.out.println(i);
+
+	}
+
+}
---
+++ chapter4/RoopEx3.java
@@ -0,0 +1,17 @@
+package chapter4;
+
+public class RoopEx3 {
+
+	public static void main(String[] args) {
+
+		for(int i = 2; i <= 10; i++) {
+			System.out.println("첫번째 반복의 " + i);
+			
+			for(int j = 9; j < 5; j++) {
+				System.out.println("두번째 반복의 " + j);
+			}
+		}
+
+	}
+
+}
