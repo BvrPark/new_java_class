b4bdc7dd50db1e2c71161ce0ef369a35068e6d36
crupy <wlxh95@naver.com>
2021-05-27 23:48:28 +0900

    chapter2 boolean

---
+++ chapter2/BooleanEx.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class BooleanEx {
+
+	public static void main(String[] args) {
+		boolean isMarried = true;
+		boolean isMarried2 = false;
+		System.out.println(isMarried);
+		System.out.println(isMarried2);
+
+	
+	}
+
+}
