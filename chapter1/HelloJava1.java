fe0ba83123aea15894e7f36d2c8229e30e73d3b2
crupy <wlxh95@naver.com>
2021-05-27 23:48:10 +0900

    chapter 1

---
+++ chapter1/HelloJava1.java
@@ -0,0 +1,28 @@
+
+package chapter1;
+
+public class HelloJava1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int level;
+level = 10;
+
+byte level2 = -12;
+
+long level4 = 12345678900L;
+
+char name = 'A';
+char name2 = 65;
+
+double level5 =  0.11111111111111111111111111;
+float  level6 =  0.11111111111111111111111111f;
+
+		System.out.println("Hello, Java"); //출력하는 함수
+        System.out.println(name);
+        System.out.println(name2);
+        System.out.println(level5);
+        System.out.println(level6);
+        System.out.println("/                           l");
+	}
+
+}
