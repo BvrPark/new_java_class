d7a7405ca169a4b84cbea369e57dcc8ef22dd1aa
crupy <wlxh95@naver.com>
2021-05-27 23:49:20 +0900

    chapter2 DoubleEx

---
+++ chapter2/DoubleEx1.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class DoubleEx1 {
+
+	public static void main(String[] args) {
+		double d_Num = 3.14;
+		float f_Num = 3.14f;
+		
+		System.out.println(d_Num);
+		System.out.println(f_Num);
+		
+	}
+
+}
