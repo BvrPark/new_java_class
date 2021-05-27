4c6c8f8a4959f8eedb689a395bb8dbfee88f70c8
crupy <wlxh95@naver.com>
2021-05-27 23:49:44 +0900

    chapter2 Explicit,ImplicitEx

---
+++ chapter2/ImplicitConversion.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class ImplicitConversion {                   //업캐스팅
+
+	public static void main(String[] args) {
+		byte b_Num = 10;
+		int i_Num = b_Num;
+		
+		System.out.println(b_Num);
+		System.out.println(i_Num);
+		
+		double d_Num;
+		d_Num = i_Num + b_Num;
+		
+		System.out.println(d_Num);
+
+	}
+
+}
