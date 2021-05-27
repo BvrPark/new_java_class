33a2fdb0860159202a966597bfd2748bfecc7c1a
crupy <wlxh95@naver.com>
2021-05-27 23:49:02 +0900

    chapter2 ConstantEx

---
+++ chapter2/Constant.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		final int MAX_NUM = 100;
+		final int MIN_NUM = 0;
+		final double PI = 3.14;
+		
+		int n_Num = MIN_NUM + 10;
+		
+	
+		
+		System.out.println(MAX_NUM);
+		System.out.println(MIN_NUM);
+		System.out.println(PI);
+		System.out.println(n_Num);
+
+	}
+
+}
