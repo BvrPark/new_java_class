000ef68fb8eb644072eaefa2af8bbae6e0b67300
crupy <wlxh95@naver.com>
2021-06-09 00:45:22 +0900

    자바 기초 출력문

---
+++ chapter1/HelloJava.java
@@ -0,0 +1,27 @@
+package chapter1;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.print("HelloJava!"); 	// ln = line을 의미
+		System.out.println("HelloJava!!");	// System = 표준을 의미
+		System.out.println("HelloJava!!!");
+		
+		System.out.println(
+				"[개인 정보 조회]\n"
+				);
+		System.out.print("이름 : ");
+		System.out.println("박지호");
+		System.out.print("성별 : ");
+		System.out.println("남");
+		System.out.print("연락처 : ");
+		System.out.println("010-5125-8841");
+		System.out.print("주소 : ");
+		System.out.println("부산 연제구");
+		System.out.println("<모든 데이터를 조회하였습니다.>");
+		System.out.println("------------------------");
+		
+		
+	}
+
+}
