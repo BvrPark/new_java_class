35e5c2fb4e7cb81b9a04accb732a2ebda3447a31
crupy <wlxh95@naver.com>
2021-06-11 00:59:49 +0900

    선증가, 후증가, 선감소, 후감소 예제 2

---
+++ chapter2/OperationEx2_2.java
@@ -0,0 +1,24 @@
+package chapter2;
+
+public class OperationEx2_2 {
+	
+	
+
+	public static void main(String[] args) {
+		int i_Num = 10;
+		int i_Num2 = 20;
+		
+		System.out.println(i_Num);
+		
+		System.out.print(++i_Num);
+		System.out.print(i_Num);
+		
+		System.out.print(i_Num++);
+		System.out.print(i_Num);
+		
+		System.out.print(i_Num);
+		System.out.print(i_Num2);
+
+	}
+
+}
