06bd29f0185bb8948d0131f1974c1f8afd226e75
crupy <wlxh95@naver.com>
2021-06-11 01:01:10 +0900

    논리 연산자 여러개 같이 쓰기

---
+++ chapter2/OperationEx4_2.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class OperationEx4_2 {
+	//番番 番番番~&&\\||
+
+	public static void main(String[] args) {
+		boolean trueFalse = true;
+		boolean trueFalse2 = true;
+		boolean trueFalse3 = true;
+		boolean trueFalse4 = false;
+		
+		System.out.println(trueFalse && trueFalse2 && trueFalse3 && trueFalse4);
+		System.out.println(trueFalse || trueFalse2 || trueFalse3 || trueFalse4);
+		System.out.println(trueFalse || trueFalse2 || trueFalse3 || trueFalse4);
+		
+
+	}
+
+}
