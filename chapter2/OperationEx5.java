20bfa6d0ce31ab41c0792437afdaa750c7dc28db
crupy <wlxh95@naver.com>
2021-06-14 01:09:01 +0900

    boolean과 삼항연산자

---
+++ chapter2/OperationEx5.java
@@ -0,0 +1,20 @@
+package chapter2;
+
+public class OperationEx5 {
+
+	public static void main(String[] args) {
+		int fatherAge = 55;
+		int motherAge = 57;
+
+		char trueFalse;
+		int trueFalse2;
+
+		trueFalse = (fatherAge > motherAge) ? 'T' : 'F';
+		trueFalse2 = (fatherAge > motherAge) ? 1 : 0;
+
+		System.out.println(trueFalse);
+		System.out.println(trueFalse2);
+
+	}
+
+}
