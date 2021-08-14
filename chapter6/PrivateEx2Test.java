082788321095590bb2fbfeb274c0b774207c0198
crupy <wlxh95@naver.com>
2021-08-14 22:37:27 +0900

    Private에 대한 2번째 예제와 그 test 클래스

---
+++ chapter6/PrivateEx2Test.java
@@ -0,0 +1,18 @@
+package chapter6;
+
+public class PrivateEx2Test {
+
+	public static void main(String[] args) {
+		PrivateEx2 pe = new PrivateEx2();
+		PrivateEx2 pe2 = new PrivateEx2();
+		
+		pe.setStudentID(201514123);
+		pe.setStudentName("¹ÚÁöÈ£");
+		pe.setStudentYear(3);
+		pe.setStudentGroup(5);
+		
+		
+
+	}
+
+}
