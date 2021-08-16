3943b7b3331f295b2564075fde6d9d2c2bc3c502
crupy <wlxh95@naver.com>
2021-08-16 23:02:24 +0900

    Student 클래스 생성후 테스트하는 클래스까지 만들어서 테스트

---
+++ chapter6/Student.java
@@ -0,0 +1,21 @@
+package chapter6;
+
+public class Student {
+	
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	void test() {
+		System.out.println("testÀÔ´Ï´Ù.");
+	}
+	public Student() {
+		studentID = 20210000;
+		
+	}
+	public Student(String stName) {
+		studentName = stName;
+	}
+
+}
