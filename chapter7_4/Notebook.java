ed00a76a435a2b66e864fdca4f1fb4729dc91181
crupy <wlxh95@naver.com>
2021-10-05 22:55:07 +0900

    computer클래스를 상속받은 Notebook클래스 생성

---
+++ chapter7_4/Notebook.java
@@ -0,0 +1,12 @@
+package chapter7_4;
+
+public class Notebook extends Computer{
+	@Override
+	public void display() {
+		System.out.println("NoteBook display()");
+	}
+	@Override
+	public void typing() {
+		System.out.println("NoteBook typing()");
+	}
+}
