d1405dc61f44e3969c0389fd36832a85f247448f
crupy <wlxh95@naver.com>
2021-11-10 23:15:59 +0900

    Generic에 대한 설명

---
+++ chapters10/Generic.java
@@ -0,0 +1,17 @@
+package chapters10;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * 제네릭(Generic) : 원하는 클래스의 형태로 클래스를 정의하는 것
+ * 클래스명<클래스 명> -> <>안의 클래스 명은 매개변수처럼 사용됨
+ * T,E,U,V 등의 형태로<>안의 클래스명을 자주 사용한다.(int i, String s 같은 느낌?)
+ * 
+ */
+
+public class Generic<T> {
+	T ani;
+	ArrayList<String> test;
+
+}
